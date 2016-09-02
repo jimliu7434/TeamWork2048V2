e3129238ae62c43fc4a877ee11f1fca25c7c4dec
jimliu <jim.liu@mitake.com.tw>
2016-09-02 18:15:04 +0800

    [modify] 事件註冊

--- server/js_serv/socket.js
+++ server/js_serv/socket.js
@@ -1,26 +1,57 @@
-﻿var gamedata = require(__dirname + '/gamedata-modules.js');
+﻿var gamedata = require(__dirname + "/gamedata-modules.js");
 
 
 
 var Modules = function() {};
 
 Modules.prototype.init = function(server) {
-    var io = require('socket.io').listen(server);
+    var io = require("socket.io").listen(server);
 
-    io.sockets.on('connection', function(socket) {
-        socket.on('reset', function(data) {
+    io.sockets.on("connection", function(socket) {
+        socket.on("reset", function(data) {
             var size = data.size;
 
-            var gd = gamedata.getData();
-
-            socket.emit('reset', { data: gd });
+            
+            socket.emit("reset", { data: size });
+        });
+        
+        socket.on("rollback", function (data) {
+            var size = data.size;
+            
+            
+            socket.emit("rollback", { data: gd });
+        });
+        
+        socket.on("up", function (data) {
+            var size = data.size;
+            
+            
+            socket.emit("up", { data: gd });
+        });
+        
+        socket.on("down", function (data) {
+            var size = data.size;
+            
+            
+            socket.emit("down", { data: gd });
+        });
+        
+        socket.on("left", function (data) {
+            var size = data.size;
+            
+            
+            socket.emit("left", { data: gd });
+        });
+        
+        socket.on("right", function (data) {
+            var size = data.size;
+            
+            
+            socket.emit("right", { data: gd });
         });
 
-
-        socket.on('test', function(data) {
-            gamedata.addData(data);
-
-            var gd = gamedata.getData();
+        socket.on("test", function(data) {
+            
         });
     });
 };
